dbc61b6da82640f0fd9fd4ed1d38ff0452a2bcec
Przemek Kulik <45256038+przemekulik@users.noreply.github.com>
2020-04-30 21:13:19 +0200

    Moved controller to separate file

---
+++ controllers/root.js
@@ -0,0 +1,7 @@
+var logger = require('../utilities/loggers')
+
+exports.root_get = function(req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify({Application: "NodeTours", Version: "2.0.0", Host: host, Port: port}));
+  res.end();
+}
--- routers/root.js
+++ routers/root.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var rootController = require('../controllers/root')
+
 router
   .route('/')
-  .get(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify({Application: "NodeTours", Version: "2.0.0", Host: host, Port: port}));
-    res.end();
-  });
+  .get(rootController.root_get);
 
 module.exports = router;
