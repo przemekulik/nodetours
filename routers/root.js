266c32187476c46cf70cc61152d1c80f0a6c6f22
Przemek Kulik <45256038+przemekulik@users.noreply.github.com>
2020-04-30 18:46:47 +0200

    changed => to function

--- routers/root.js
+++ routers/root.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router
   .route('/')
-  .get((req, res, next) => {
+  .get(function(req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.write(JSON.stringify({Application: "NodeTours", Version: "2.0.0", Host: host, Port: port}));
     res.end();
