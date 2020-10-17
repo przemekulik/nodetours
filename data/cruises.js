8a90c40a5012c0ea91d2f281045840f796146677
Przemek Kulik <45256038+przemekulik@users.noreply.github.com>
2020-10-17 18:50:32 +0200

    Cruises data model change

--- data/cruises.js
+++ data/cruises.js
@@ -3,7 +3,39 @@ const logger = require('../utilities/loggers')
 const Cruises = function() {
   // GET all
   this.getCruises = function(dbo, callback) {
-    dbo.collection('cruises').find({}, {projection:{_id: 0}}).toArray(function(err, data) {
+    dbo.collection('cruises_new').aggregate([
+      { $lookup: {from: 'rooms', localField: 'roomTypes.roomID', foreignField: 'roomID', as: 'roomTypes'} },
+      {
+        $addFields: {
+          roomTypes: {
+            $map: {
+              input: "$roomTypes",
+              in: {
+                $mergeObjects: [
+                  "$$this",
+                  {
+                    available: {
+                      $reduce: {
+                        input: "$$this.capacity",
+                        initialValue: 0,
+                        in: {
+                          $cond: [
+                            { $eq: ["$$this.cruiseID", "$cruiseID"] },
+                            "$$this.available",
+                            "$$value"
+                          ]
+                        }
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      },
+      { $project: {_id: 0, 'roomTypes': {_id: 0}, 'roomTypes.capacity': 0} }
+    ]).toArray(function(err, data) {
       if (err) {
         logger.error(`getCruises : Error reading cruises db`);
         logger.error(`  Error: ${err}`);
@@ -17,9 +49,42 @@ const Cruises = function() {
   
   // GET by {id}
   this.getCruise = function(dbo, id, callback) {
-    dbo.collection("cruises").findOne({'cruiseID': id}, {projection:{_id: 0}}, function (err, data) {
+    dbo.collection('cruises_new').aggregate([
+      { $match: {'cruiseID': id} },
+      { $lookup: { from: 'rooms', localField: 'roomTypes.roomID', foreignField: 'roomID', as: 'roomTypes'} },
+      {
+        $addFields: {
+          roomTypes: {
+            $map: {
+              input: "$roomTypes",
+              in: {
+                $mergeObjects: [
+                  "$$this",
+                  {
+                    available: {
+                      $reduce: {
+                        input: "$$this.capacity",
+                        initialValue: 0,
+                        in: {
+                          $cond: [
+                            { $eq: ["$$this.cruiseID", "$cruiseID"] },
+                            "$$this.available",
+                            "$$value"
+                          ]
+                        }
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      },
+      { $project: {_id: 0, 'roomTypes': {_id: 0}, 'roomTypes.capacity': 0} }
+    ]).toArray(function(err, data) {
       if (err) {
-        logger.error(`getCruise(id): Error reading cruises db`);
+        logger.error(`getCruises : Error reading cruises db`);
         logger.error(`  Error: ${err}`);
         callback(err, null);
       } else {
